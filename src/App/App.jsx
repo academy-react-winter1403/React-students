a5c84ff9a2a9997e612acba2c00fb075a6b6e1f4
Matin Poormoradi <matinpoormoradi8@gmail.com>
2025-03-19 13:02:24 +0330

    clean and added interceptor and api func

---
+++ src/App/App.jsx
@@ -0,0 +1,19 @@
+import Header from '../components/common/Header'
+import Content from '../components/content/Content'
+import Footer from '../components/footer/Footer'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <Header/>
+      <Content/>
+      <Footer/>
+      {
+        //https://www.figma.com/design/17vAsBH5Aa6uw69eoq9iP0/React-Students?node-id=0-1&t=YnVcTRpFjO6Y3e9s-1
+      }
+    </>
+  )
+}
+
+export default App
