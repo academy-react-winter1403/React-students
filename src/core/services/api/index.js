aecc1323dee3a3f0cac1ef863ceeb6ef468cc392
Matin Poormoradi <matinpoormoradi8@gmail.com>
2025-03-19 13:02:50 +0330

    clean and added interceptor and api func

---
+++ src/core/services/api/index.js
@@ -0,0 +1,12 @@
+import http from "./interceptor";
+
+const getApi = async (url) => {
+  try {
+    const response = await http.get(url);
+    return response;
+  } catch (error) {
+    return console.log(error);
+  }
+};
+
+export { getApi };
