df0890ea427d30f32197bc1d836fdeea07f6d26b
Parsa <mparsabahredar@gmail.com>
2025-03-21 20:37:15 +0330

    installing tailwindcss version 3 and include iranyekan font

---
+++ src/components/content/HeroSection.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const HeroSection = () => {
+  return (
+    <div>
+        <div className="content1  w-[100%] h-[250px] pt-[60px] text-center border">
+            <p className="text-[25px]">با آکادمی سپهر میتونی از کدنویسی<span> نهایت لذت </span>رو ببری</p>
+            <p className="mt-[10px] text-[12px] text-gray-500">آکادمی سپهر همیشه در تلاش بوده تا محیطی پرانرژی را برای علاقه مندان به حوزه برنامه نویسی و کدنویسی و طراحی وب سایت را فراهم کند.</p>
+            <div className="flex w-[15%] h-[40px] justify-center items-center gap-[20px] mx-[auto] mt-[20px] border">
+            <button className="w-[38%] h-[30px] text-[12px] text-white bg-[#A74DD1] rounded-[10px]">شروع یادگیری</button>
+            <button className="w-[40%] h-[30px] text-[12px] border rounded-[10px]">درباره ما</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default HeroSection
