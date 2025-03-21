95d7ba868b19ae4574f7a8386516570c83033c67
Parsa <mparsabahredar@gmail.com>
2025-03-22 00:17:05 +0330

    by Parsa

--- src/components/common/Header.jsx
+++ src/components/common/Header.jsx
@@ -8,17 +8,17 @@ const Header = () => {
     <div className="header  flex justify-center items-center  gap-[350px] h-[100px] border">
       <div className="logo  flex items-center gap-[10px]">
         <img src="" alt=""/>
-        <h2 className="text-[13px]">React-students</h2>
+        <h2 className="text-[14px]">React-students</h2>
       </div>
       <div className="links   flex justify-center items-center gap-[30px] w-[30%] h-[50px] border">
-        <a className="px-[10px] py-[5px] text-[13px] rounded-[10px]  hover:bg-gray-200">خانه</a>
-        <a className="px-[10px] py-[5px] text-[13px] rounded-[10px]  hover:bg-gray-200">دوره ها</a>
-        <a className="px-[10px] py-[5px] text-[13px] rounded-[10px]  hover:bg-gray-200">بلاگ ها</a>
-        <a className="px-[10px] py-[5px] text-[13px] rounded-[10px]  hover:bg-gray-200">درباره ما</a>
+        <a className="px-[10px] py-[5px] text-[16px] rounded-[10px]  hover:bg-gray-200">خانه</a>
+        <a className="px-[10px] py-[5px] text-[16px] rounded-[10px]  hover:bg-gray-200">دوره ها</a>
+        <a className="px-[10px] py-[5px] text-[16px] rounded-[10px]  hover:bg-gray-200">بلاگ ها</a>
+        <a className="px-[10px] py-[5px] text-[16px] rounded-[10px]  hover:bg-gray-200">درباره ما</a>
       </div>
       <div className="loginSignup  w-[9%] h-[60px] flex justify-center items-center gap-[10px] border">
         <a className="flex justify-center items-center w-[35%] h-[30px] text-[13px] border rounded-[10px]">ورود</a>
-        <a className="flex justify-center items-center w-[55%] h-[30px] text-[13px] text-white bg-[#A74DD1] rounded-[10px]">ثبت نام</a>
+        <a className="flex justify-center items-center w-[83px] h-[37px] px-[16px] py-[8px] text-[16px] text-white bg-[#A74DD1] rounded-[12px]">ثبت نام</a>
       </div>
     </div>
   )
--- src/components/content/HeroSection.jsx
+++ src/components/content/HeroSection.jsx
@@ -3,16 +3,16 @@ import React from 'react'
 const HeroSection = () => {
   return (
     <div>
-        <div className="content1  w-[100%] h-[280px] pt-[60px] text-center border">
-          <div className="w-[35%] mx-[auto] border">
-            <p className="text-[25px]">با آکادمی سپهر میتونی از کدنویسی
+        <div className="content1  w-[584px] h-[261px] pt-[60px] text-center border">
+          <div className="w-[584px] h-[134px] mx-[auto] border">
+            <p className="text-[40px]">با آکادمی سپهر میتونی از کدنویسی
               <span className="mx-[5px] px-[5px] py-[5px] text-[#A74DD1] bg-[url('../public/Group 1.svg')] border-[3px] border-[#A74DD1]"> نهایت لذت </span>
             رو ببری</p>
-            <p className="mt-[10px] text-[12px] leading-[25px] text-gray-500">آکادمی سپهر همیشه در تلاش بوده تا محیطی پرانرژی را برای علاقه مندان به حوزه برنامه نویسی و کدنویسی و طراحی وب سایت را فراهم کند.</p>
+            <p className="w-[547px] h-[42px] mt-[10px] text-[12px] leading-[100%] text-gray-500">آکادمی سپهر همیشه در تلاش بوده تا محیطی پرانرژی را برای علاقه مندان به حوزه برنامه نویسی و کدنویسی و طراحی وب سایت را فراهم کند.</p>
           </div>
             <div className="flex w-[15%] h-[40px] justify-center items-center gap-[20px] mx-[auto] mt-[20px] border">
-            <button className="w-[38%] h-[30px] text-[12px] text-white bg-[#A74DD1] rounded-[10px]">شروع یادگیری</button>
-            <button className="w-[40%] h-[30px] text-[12px] border rounded-[10px]">درباره ما</button>
+            <button className="flex justify-center items-center w-[124px] h-[45px] px-[12px] py-[16px] text-[16px] text-white bg-[#A74DD1] rounded-[16px]">شروع یادگیری</button>
+            <button className="w-[86px] h-[45px] px-[16px] py-[12px] text-[12px] border rounded-[10px]">درباره ما</button>
         </div>
       </div>
     </div>
