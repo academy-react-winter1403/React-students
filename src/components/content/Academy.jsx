932aff3f9644b9e709390809abc6b729a8f4a43c
Parsa <mparsabahredar@gmail.com>
2025-03-21 20:51:32 +0330

    تغییر جزئی در استایل HeroSection.jsx , Academy.jsx

--- src/components/content/Academy.jsx
+++ src/components/content/Academy.jsx
@@ -5,7 +5,7 @@ const Academy = () => {
     <div>
       <div className="content3  flex h-[400px] border">
         <div className="w-[50%] h-[400px] px-[80px]">
-          <button className="w-[12%] h-[30px] mt-[10px] text-[12px] text-[#A74DD1] bg-[#F1D1FF] rounded-[10px]">درباره آکادمی</button>
+          <button className="w-[15%] h-[30px] mt-[10px] text-[12px] text-[#A74DD1] bg-[#F1D1FF] rounded-[10px]">درباره آکادمی</button>
           <h2 className="mt-[10px] text-[25px]">آکادمی سپهر</h2>
           <p className="mt-[20px] text-[15px]">آکادمی سپهر با 20 سال سابقه آموزش تخصصی در زمینه کدنویسی و برنامه نویسی در سنین کودکی تا بزرگسال در محیطی جامع و پیشرفت متداول در تمامی مهارت ها در کنار اساتید بزرگ از سراسر جهان. اینجا مکانی برای بدست اوردن رویاهای شماست.</p>
         </div>
