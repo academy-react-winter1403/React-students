85fba2969bf90eea62de7f48306545aa8eb943bc
Nazanin <rnazanin175@gmail.com>
2025-04-25 17:18:27 +0330

    add coursetext

---
+++ src/components/CourseDetail/CourseText.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CourseText = () => {
+  return (
+    <div>
+        <p className='lg:mt-[24px] md:mt-[10px] sm:w-[57px] sm:h-[25px] sm:text-[10px] md:w-[66px] flex items-center justify-center lg:w-[69px] lg:text-[13px] md:text-[12px] h-[26px] text-[#A74DD0] bg-[#F1D1FF] rounded-lg text-[12px]'>
+                              اسم دوره
+        </p>
+    </div>
+  )
+}
+
+export default CourseText
